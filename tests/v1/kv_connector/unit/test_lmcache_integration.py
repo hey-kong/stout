b1c032806111083d4b10bed42071cb1f0de1cfc5
Iggie Wang <iggiewang@gmail.com>
2026-02-28 23:36:52 +0800

    Rollback code to 1892993bc18e243e2c05841314c5e9c06a80c70d

--- tests/v1/kv_connector/unit/test_lmcache_integration.py
+++ tests/v1/kv_connector/unit/test_lmcache_integration.py
@@ -83,7 +83,6 @@ def test_config_interface():
 
     assumes(KVTransferConfig, "kv_role")
     assumes(KVTransferConfig, "kv_connector_extra_config")
-    assumes(KVTransferConfig, "save_decode_cache", is_instance_of=bool)
 
     assumes(ModelConfig, "use_mla", is_instance_of=bool)
     assumes(ModelConfig, "dtype")
